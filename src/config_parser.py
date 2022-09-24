f03b3775f4c860084cbad4f9a686b1b9882a6aba
WatermelonSalt <watermelonsalt682@gmail.com>
2022-09-24 19:36:44 +0530

    partial commit to sync online editor

--- src/config_parser.py
+++ src/config_parser.py
@@ -24,24 +24,87 @@ class Configurator:
 
         for dir in self.paths["dir"]:
 
-            yield dir
+            if isdir(dir):
 
-            logger.logger.debug(f"Facilitated path to {dir} in mode 'dir'")
+                yield dir
+
+                logger.logger.debug(f"Facilitated path to {dir} in mode 'dir'")
+
+            else:
+
+                logger.logger.debug(
+                    f"{dir} is not a directory, did not facilitate path")
 
     def encapsulating_facilitator(self):
 
         for path in self.paths["encapsulating"]:
 
-            for dir in listdir(path):
+            if isdir(path):
 
-                if isdir(f"{path}/{dir}"):
+                yield path
 
-                    yield dir
+                for dir in listdir(path):
+
+                    if isdir(f"{path}/{dir}"):
+
+                        yield dir
+
+                        logger.logger.debug(
+                            f"Facilitated path to {path}/{dir} in mode 'encapsulating'")
+
+                    else:
+
+                        logger.logger.debug(
+                            f"{path}/{dir} is not a directory, did not facilitate path")
+
+            else:
+
+                logger.logger.debug(
+                    f"Skipping {path} as it is not a directory"
+                )
+
+    def recurse_facilitator(self):
+
+        dirs_to_recurse = []
+
+        for path in self.paths["recurse"]:
+
+            if isdir(path):
+
+                dirs_to_recurse.append(path)
+
+                logger.logger.debug(
+                    f"Added {path} to the list of directories to be recursed"
+                )
+
+            else:
+
+                logger.logger.debug(
+                    f"Skipping {path} as it is not a directory"
+                )
+
+        while dirs_to_recurse:
+
+            yield dirs_to_recurse[0]
+
+            logger.logger.debug(
+                f"Facilitated path to {dirs_to_recurse[0]} in mode 'recurse'"
+            )
+
+            for dir in listdir(dirs_to_recurse[0]):
+
+                if isdir(f"{dirs_to_recurse[0]}/{dir}"):
+
+                    dirs_to_recurse.append(f"{dirs_to_recurse[0]}/{dir}")
 
                     logger.logger.debug(
-                        f"Facilitated path to {dir} in mode 'encapsulating'")
+                        f"Added {dirs_to_recurse[0]}/{dir} to the list of directories to be recursed"
+                    )
 
                 else:
 
                     logger.logger.debug(
-                        f"{dir} is not a directory, did not facilitate path")
+                        f"Skipping {dirs_to_recurse[0]}/{dir} as it is not a directory"
+                    )
+
+            dirs_to_recurse.pop(0)
